4a52bf966349896964e5803fc532e2eb267f05ad
Levikov <leevick.zju@foxmail.com>
2016-05-05 17:39:59 +0800

    Add data operation class

---
+++ libDataOperation/Class1.cs
@@ -0,0 +1,29 @@
+﻿#region 引用
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace libDataOperation
+{
+    #region 数据分包类
+    public class dataDistribute
+    {
+        public dataDistribute()
+        {
+
+        }
+
+        public static void readData()
+        { }
+        public void writeDatabase()
+        {
+
+        }
+
+    }
+    #endregion
+
+}
