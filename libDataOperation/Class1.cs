c4eb2263fbfeea400fc0f7c04a4965399ce5b5a6
Levikov <leevick.zju@foxmail.com>
2016-05-08 09:58:13 +0800

    ADD FUNCTION

--- libDataOperation/Class1.cs
+++ libDataOperation/Class1.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
+using GroundAnalyser;
+using System.IO;
 #endregion
 namespace GroundAnalyser.Data
 {
+
 
     #region 数据解包
 
@@ -24,18 +27,53 @@ namespace GroundAnalyser.Data
 
     public class Distribute
     {
-        public Distribute()
+        private string strFilePath;
+        private OleDbConnection conn;
+        public Distribute(string FilePath, string dbPath)
         {
-
+            strFilePath = FilePath;
+            string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Victor\OneDrive\Code\CSharp\GroundAnalyser\GroundAnalyser\data\db.mdb";
+            conn = new OleDbConnection(conString);
+            conn.Open();
         }
 
-        public static void readData()
+        public void importData()
         {
+            FileStream fileData = new FileStream(strFilePath, FileMode.Open);
+            byte[] bufFile = new byte[fileData.Length];
+            byte[] row = new byte[280];
+            fileData.Read(bufFile, 0, (int)fileData.Length);
+
+            for (int i = 0; 280 * i < fileData.Length; i++)
+            {
+                Array.Copy(bufFile, 280 * i, row, 0, 280);
+                insertData(row);
+                
+            }
+
+            fileData.Close();
+            
+
 
         }
-        public void writeDatabase()
+        public void insertData(byte[] row)
         {
 
+            //string strCommand = "INSERT INTO AuxData (FrmCnt) VALUES('" + BitConverter.ToUInt16(row, 48) + "')";
+            if (row[4] == 0x08)
+            {
+                byte strip = row[5];
+                Console.WriteLine("条带号:" + row[5].ToString());
+
+                byte[] FrmNum = { row[7], row[6] };
+                Console.WriteLine("包计数:" + BitConverter.ToUInt16(FrmNum,0).ToString()); 
+
+
+                Console.WriteLine(row[6] * 256 + row[7]);
+            }
+            //OleDbCommand cmd = new OleDbCommand(strCommand, conn);
+            //cmd.ExecuteScalar();
+            
         }
 
     }
--- libraryTest/Program.cs
+++ libraryTest/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GroundAnalyser.Data;
+using System.Data.OleDb;
+using System.IO;
 
 namespace libraryTest
 {
@@ -11,6 +13,9 @@ namespace libraryTest
     {
         static void Main(string[] args)
         {
+            Distribute dist = new Distribute(@"D:\Project\data\第二次改完程序_正确_280.raw", @"C:\Users\Victor\OneDrive\Code\CSharp\GroundAnalyser\GroundAnalyser\data\db.mdb");
+            dist.importData();
+            
         }
     }
 }
